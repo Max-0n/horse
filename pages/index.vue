3c0d4fdb5c925d604ec151fa992298d108f4dd68
ILALEX <79199806+jean-louis1776@users.noreply.github.com>
2025-10-30 17:10:44 +0300

    Merge pull request #8
    * fix
    
    * Merge branch 'main' into ilya-dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  NuxtLink(to="/game") START
+  .game-container
+    h1.game-title Horse Defied
+    img(src="/images/game_logo.png" alt="Horse Defied").game-logo
+    NuxtLink(to="/game").game-start START
 </template>
 
 <script lang="ts" setup>
@@ -7,5 +10,38 @@
 </script>
 
 <style lang="scss" scoped>
-
+.game-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 20px;
+}
+.game-title {
+  font-size: 64px;
+  font-weight: 700;
+  text-align: center;
+  text-transform: uppercase;
+  max-width: 250px;
+}
+.game-logo {
+  width: 300px;
+  height: auto;
+}
+.game-start {
+  font-size: 40px;
+  font-weight: 700;
+  color: #000000;
+  padding: 8px 20px;
+  border: 6px solid #000;
+  text-decoration: none;
+  outline: none;
+  border-radius: 40px;
+  transition: all 0.3s ease;
+  &:hover {
+    background: #000000;
+    color: #ffffff;
+  }
+}
 </style>
